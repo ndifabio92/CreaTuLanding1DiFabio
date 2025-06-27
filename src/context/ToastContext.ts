fe34dbbd9c0629eb6cbdb6d7f26f00ffa82a3875
Nicolás Di Fabio <ndifabio@icebergsolutions.com>
2025-06-27 02:01:51 -0300

    se arregla dependencia

--- src/context/ToastContext.ts
+++ src/context/ToastContext.ts
@@ -1,5 +1,6 @@
 import { createContext } from "react";
-import { ToastContextType } from "../../types/ui/toast";
+import { ToastContextType } from "../types/ui/toast";
+
 
 export const ToastContext = createContext<ToastContextType | undefined>(
   undefined
